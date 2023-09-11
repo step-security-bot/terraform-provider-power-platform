baf00b202a122134860603408bb8ad032d98a776
Jian Sun <jian.sun@microsoft.com>
2023-09-11 22:19:51 +0000

    Successful creation of dataverse environment and data lake storage account gen 2.

--- examples/quickstarts/301-synpase-link-for-dataverse/main.tf
+++ examples/quickstarts/301-synpase-link-for-dataverse/main.tf
@@ -1,35 +1,56 @@
 terraform {
-  required_version = ">= 1.5"
   required_providers {
     powerplatform = {
-      source = "microsoft/power-platform"
+      version = ">= 0.1"
+      source  = "microsoft/power-platform"
     }
     azurerm = {
-      source = "hashicorp/azurerm"
+      version = ">= 1.5"
+      source  = "hashicorp/azurerm"
     }
   }
 }
 
 provider "powerplatform" {
-  username  = var.username
-  password  = var.password
+  client_id = var.client_id
+  secret    = var.client_secret
   tenant_id = var.tenant_id
 }
 
-
 resource "powerplatform_environment" "development" {
-  display_name      = "example_environment"
-  location          = "europe"
-  language_code     = "1033"
-  currency_code     = "USD"
-  environment_type  = "Sandbox"
-  domain            = "mydomain"
-  security_group_id = "00000000-0000-0000-0000-000000000000"
+  display_name      = var.dataverse_env_display_name
+  location          = var.dataverse_location
+  language_code     = var.dataverse_language_code
+  currency_code     = var.dataverse_currency_code
+  environment_type  = var.dataverse_environment_type
+  domain            = var.tenant_domain
+  security_group_id = var.dataverse_security_group_id
 }
 
-data "powerplatform_connectors" "all_connectors" {}
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
 
-data "azurerm_resource_group" "example" {
-  name     = "example"
-  location = "West Europe"
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-resources"
+  location = var.location
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "${var.prefix}storageacct"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  network_rules {
+    default_action = "Deny"
+    ip_rules       = ["23.45.1.0/30"]
+  }
 }
--- examples/quickstarts/301-synpase-link-for-dataverse/variables.tf
+++ examples/quickstarts/301-synpase-link-for-dataverse/variables.tf
@@ -1,26 +1,70 @@
-variable "username" {
-  description = "The username of the Power Platform API in user@domain format"
-  type        = string
-  required    = false
-  default     = null
-  validation {
-    condition     = (var.username != null && var.password != null) || (var.username == null && var.password == null)
-    error_message = "Both username and password must be provided, or both should be null."
-  }
-}
-variable "password" {
-  description = "The password of the Power Platform API user"
-  sensitive   = true
-  type        = string
-  default     = null
-  required    = false
-  validation {
-    condition     = (var.username != null && var.password != null) || (var.username == null && var.password == null)
-    error_message = "Both username and password must be provided, or both should be null."
-  }
-}
 variable "tenant_id" {
   description = "The AAD tenant id of service principal or user"
   type        = string
-  required    = true
+  default     = "fb390a9f-b556-4773-bb8b-8087b18b16b2"
+}
+
+variable "client_id" {
+  description = "The client ID of the of the service principal"
+  default     = "a4df11c8-c696-47e4-97cd-6fa95810660b"
+  type        = string
+
+}
+variable "client_secret" {
+  description = "The client secret of the service principal"
+  sensitive   = true
+  type        = string
+  default     = "39r8Q~xXji5zyzdn0HT1RfNUC7YMK4rjqYxuzbKK"
+}
+
+variable "subscription_id" {
+  description = "The Azure subscription for ASLG2 deployment"
+  default     = "891329ff-74ec-491c-8ccf-eb052af54740"
+  sensitive   = true
+  type        = string
+}
+
+variable "prefix" {
+  default     = "terraformppp"
+  description = "The Prefix used for all resources in this example"
+}
+
+variable "location" {
+  default     = "westeurope"
+  description = "The Azure Region in which all resources in this example should be created."
+}
+
+variable "dataverse_env_display_name" {
+  default     = "synapse_link"
+  description = "The display name for the dataverse environment."
+}
+
+variable "dataverse_location" {
+  default     = "europe"
+  description = "Region where the dataverse instance is provisioned."
+}
+
+variable "dataverse_language_code" {
+  default     = "1033"
+  description = "Language code for Dataverse."
+}
+
+variable "dataverse_currency_code" {
+  default     = "USD"
+  description = "Language code for Dataverse."
+}
+
+variable "dataverse_environment_type" {
+  default     = "Production"
+  description = "Environment type for the dataverse to reside in."
+}
+
+variable "tenant_domain" {
+  default     = "mngenvmcap080290"
+  description = "Lowercase domain name for the tenant."
+}
+
+variable "dataverse_security_group_id" {
+  default     = "00000000-0000-0000-0000-000000000000"
+  description = "Security group id to be granted access to the Dataverse."
 }
